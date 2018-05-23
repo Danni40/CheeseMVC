c86fa34d9bbd8c8c861df954be42754b43760dc5
dmoeller <dmoeller@marketvolt.com>
2018-05-23 09:01:47 -0500

    Add project files.

---
+++ CheeseMVC/Controllers/CheeseController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CheeseMVC.Controllers
+{
+    public class CheeseController : Controller
+    {
+		private static Dictionary<string, string> cheeses = new Dictionary<string, string>();
+
+		[HttpGet]
+        public IActionResult Index()
+        {
+			ViewBag.cheeses = cheeses;
+            return View();
+        }
+
+		[HttpGet]
+		public IActionResult Add() 
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Add(string cheese, string description) 
+		{
+			if (cheeses.ContainsKey(cheese))
+				cheeses[cheese] = description;
+			else
+				cheeses.Add(cheese, description);
+			return Redirect("Index");
+		}
+
+		[HttpGet]
+		public IActionResult CheckBoxDelete() 
+		{
+			ViewBag.cheeses = cheeses;
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult CheckBoxDelete(string[] cheese) 
+		{
+			return Redirect("Index");
+		}
+
+		[HttpGet]
+		public IActionResult DropDownDelete() 
+		{
+			ViewBag.cheeses = cheeses;
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult DropDownDelete(string cheese) 
+		{
+			return Redirect("Index");
+		}
+    }
+}
